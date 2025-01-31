52c52e948a798aa00781f8d427598c7e6d275ba1
AdhamPolad <edhempoladzade2@gmail.com>
2025-01-31 20:32:28 +0400

    Add student controller

---
+++ LoginSln/LoginSln/Login/Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Controllers
+{
+    public class StudentController : Controller
+    {
+        [Authorize]
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
---
+++ LoginSln/LoginSln/Login/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Login.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login.Data
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+    }
+}
